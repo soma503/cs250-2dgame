e21510edd03733ee16628a49c89521d3de6e6351
Julian To <julian.to2023@gmail.com>
2025-02-10 10:52:31 -0800

    moved platforms to level scene

--- player.gd
+++ player.gd
@@ -7,11 +7,15 @@ const SPEED = 300.0
 #jump
 var has_double_jump = true
 const JUMP_FORCE = 600
+const DOUBLE_JUMP_FORCE = 900
 
 #dash
 const DASH_SPEED = 1200
 @onready var dash = $Dash
 var dash_duration = 0.2	
+
+#camera
+@onready var camera := $Camera2D as Camera2D
 
 func _process(delta):
 	direction.x = Input.get_axis("left", "right")
@@ -20,7 +24,7 @@ func _physics_process(delta: float) -> void:
 	if !is_on_floor():
 		velocity.y += GRAVITY
 		if velocity.y > 1000:
-			velocity.y > 1000
+			velocity.y = 1000
 	
 	if Input.is_action_just_pressed("jump"):
 		_try_jump()
@@ -29,7 +33,7 @@ func _physics_process(delta: float) -> void:
 		dash.start_dash(dash_duration)
 	
 	if dash.is_dashing():
-		velocity.x =direction.x * DASH_SPEED
+		velocity.x = direction.x * DASH_SPEED
 	else:
 		velocity.x = direction.x * SPEED #move horizontally
 		
@@ -39,7 +43,6 @@ func _try_jump():
 	if is_on_floor(): #if player is on the floor, js jump
 		velocity.y = -JUMP_FORCE
 		has_double_jump = true
-	else: 
-		if has_double_jump: 
-			velocity.y = -JUMP_FORCE
-			has_double_jump = false
+	elif has_double_jump: 
+		velocity.y = -DOUBLE_JUMP_FORCE
+		has_double_jump = false
