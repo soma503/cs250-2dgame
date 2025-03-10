20ef6156b6ab5705db4d2f334338afa1fcd135bd
Julian To <julian.to2023@gmail.com>
2025-03-10 12:15:21 -0700

    fixed acceleration and friction
    friction is now applied when not holding a directional key AND holding
    opposite directional key

--- src/player/player.gd
+++ src/player/player.gd
@@ -18,35 +18,63 @@ var run_tap_interval = 5.00
 @onready var dash = $Dash
 var dash_duration = 0.2	
 
+#stuff
+var speed = 400.0
+var acceleration = 600.0
+var friction = 3000.0
+var gravity = 1900.0
+var jump_force = 600
+var double_jump_force = 900
+
+
+
 #camera
 @onready var camera := $Camera2D as Camera2D
 
-func _process(delta):
-	direction.x = Input.get_axis("left", "right")
-
 func _physics_process(delta: float) -> void:
-	if !is_on_floor():
-		velocity.y += GRAVITY
-		if velocity.y > 1000:
-			velocity.y = 1000
+	apply_gravity(delta)
+	handle_jump()
+	handle_dash()
 	
-	if Input.is_action_just_pressed("jump"):
-		_try_jump()
-			
-	if Input.is_action_just_pressed("dash") and dash.can_dash and !dash.is_dashing(): 
-		dash.start_dash(dash_duration)
+	var input_axis = Input.get_axis("left", "right")
+	handle_acceleration(input_axis, delta)
+	apply_friction(input_axis, delta)
 	
-	if dash.is_dashing():
-		velocity.x = direction.x * DASH_SPEED
-	else:
-		velocity.x = direction.x * SPEED #move horizontally
-		
 	move_and_slide()
 
-func _try_jump():
-	if is_on_floor(): #if player is on the floor, js jump
-		velocity.y = -JUMP_FORCE
-		has_double_jump = true
-	elif has_double_jump: 
-		velocity.y = -DOUBLE_JUMP_FORCE
-		has_double_jump = false
+func apply_gravity(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+func handle_dash():
+	if Input.is_action_just_pressed("dash") and dash.can_dash and !dash.is_dashing(): 
+		dash.start_dash(dash_duration)
+		
+func handle_jump():
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor(): #if player is on the floor, js jump
+			velocity.y = -JUMP_FORCE
+			has_double_jump = true
+			
+		elif has_double_jump: 
+			velocity.y = -DOUBLE_JUMP_FORCE
+			has_double_jump = false
+			
+func handle_acceleration(input_axis, delta):
+	if input_axis != 0:
+		if dash.is_dashing():
+			velocity.x = move_toward(velocity.x, DASH_SPEED * input_axis, acceleration * 10 * delta)
+		else:
+			velocity.x = move_toward(velocity.x, speed * input_axis, acceleration * delta)
+
+func apply_friction(input_axis, delta):
+	if input_axis == 0 or get_sign(input_axis) != get_sign(velocity.x):
+		velocity.x = move_toward(velocity.x, 0, friction * delta)
+
+func get_sign(number):
+	if number > 0:
+		return 1
+	else:
+		return -1
+		
+	
