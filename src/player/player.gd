b841803efde6bf123d122f8e763cf76a9b93f858
Julian To <julian.to2023@gmail.com>
2025-03-10 12:55:38 -0700

    added wall jump function
    must be holding directional key into wall and press jump to wall jump

--- src/player/player.gd
+++ src/player/player.gd
@@ -20,7 +20,9 @@ var dash_duration = 0.2
 
 #stuff
 var speed = 400.0
-var acceleration = 600.0
+var acceleration = 800.0
+var air_acceleration = 600.0
+var air_friction = 500.0
 var friction = 3000.0
 var gravity = 1900.0
 var jump_force = 600
@@ -34,11 +36,14 @@ var double_jump_force = 900
 func _physics_process(delta: float) -> void:
 	apply_gravity(delta)
 	handle_jump()
+	handle_wall_jump()
 	handle_dash()
 	
 	var input_axis = Input.get_axis("left", "right")
 	handle_acceleration(input_axis, delta)
+	handle_air_acceleration(input_axis, delta)
 	apply_friction(input_axis, delta)
+	apply_air_resistance(input_axis, delta)
 	
 	move_and_slide()
 
@@ -56,20 +61,44 @@ func handle_jump():
 			velocity.y = -JUMP_FORCE
 			has_double_jump = true
 			
-		elif has_double_jump: 
+		elif has_double_jump and not is_on_wall(): 
 			velocity.y = -DOUBLE_JUMP_FORCE
 			has_double_jump = false
+
+func handle_wall_jump():
+	if not is_on_wall(): return
+	if is_on_floor(): return
+	var wall_normal = get_wall_normal()
+	if Input.is_action_pressed("right") and Input.is_action_just_pressed("jump") and wall_normal == Vector2.LEFT:
+		velocity.x = wall_normal.x * speed
+		velocity.y = -DOUBLE_JUMP_FORCE
+	if Input.is_action_pressed("left") and Input.is_action_just_pressed("jump") and wall_normal == Vector2.RIGHT:
+		velocity.x = wall_normal.x * speed
+		velocity.y = -DOUBLE_JUMP_FORCE
+		
+func handle_air_acceleration(input_axis, delta):
+	if is_on_floor(): return
+	if input_axis != 0:
+		if dash.is_dashing():
+			velocity.x = move_toward(velocity.x, DASH_SPEED * input_axis, air_acceleration * 10 * delta)
+		else:
+			velocity.x = move_toward(velocity.x, speed * input_axis, air_acceleration * delta)
 			
 func handle_acceleration(input_axis, delta):
+	if not is_on_floor(): return
 	if input_axis != 0:
 		if dash.is_dashing():
 			velocity.x = move_toward(velocity.x, DASH_SPEED * input_axis, acceleration * 10 * delta)
 		else:
 			velocity.x = move_toward(velocity.x, speed * input_axis, acceleration * delta)
-
+			
 func apply_friction(input_axis, delta):
-	if input_axis == 0 or get_sign(input_axis) != get_sign(velocity.x):
+	if (input_axis == 0 or get_sign(input_axis) != get_sign(velocity.x)) and is_on_floor():
 		velocity.x = move_toward(velocity.x, 0, friction * delta)
+		
+func apply_air_resistance(input_axis, delta):
+	if (input_axis == 0 or get_sign(input_axis) != get_sign(velocity.x)) and not is_on_floor():
+		velocity.x = move_toward(velocity.x, 0, air_friction * delta)
 
 func get_sign(number):
 	if number > 0:
