565e22a9cb7c5fdc1e26f6fc0117e2d933e7810d
itsmelbell <melissatan723@gmail.com>
2025-04-21 11:28:36 -0700

    added coin

--- src/level/objects/coin.gd
+++ src/level/objects/coin.gd
@@ -8,7 +8,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	%Sprite2D.play("default")
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
