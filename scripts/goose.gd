e9e7e7ecb76ec953ef58f0d8237d6e48bec6ac20
soma503 <julian.to2023@gmail.com>
2025-04-28 10:34:37 -0700

    Added player detect radius

--- scripts/goose.gd
+++ scripts/goose.gd
@@ -5,16 +5,18 @@ const JUMP_VELOCITY = -400.0
 const PATROL_SPEED = 300.0
 var on_ledge = false
 var dir = -1
+var sees_player = false
 
 @onready var rand_timer = $RandomMovementTimer  #prob not needed
-@onready var left_ray = $AI/ray_left
-@onready var right_ray = $AI/ray_right
+@onready var left_ray = $ray_left
+@onready var right_ray = $ray_right
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not GameManager.is_paused:
-		if sees_entity(null):
-			aggro_towards(null)
+		if sees_player:
+			print("SEES PLAYER SEES PLAYER!!!!!")
+			aggro_towards()
 		else:
 			patrol()
 			apply_gravity(delta)
@@ -22,7 +24,7 @@ func _physics_process(delta: float) -> void:
 		move_and_slide()
 		
 
-#patrol
+# patrol
 #Changes motion of entity depending on which direction it is facing and if it on a ledge
 func patrol():
 	update_direction()
@@ -31,33 +33,31 @@ func patrol():
 		
 
 # update_direction
-# Gets the direction of entity depending on if it has hit the end of a ledge
+# Gets the direction of entity depending on if it has hit the end of a ledge and updates it
 func update_direction():
 	if !left_ray.is_colliding():
 		dir = 1 #going right
 	if !right_ray.is_colliding():
 		dir = -1 #going left
 
-# move
-# Takes a speed and direction and moves an entity at that speed and direction
-func move(speed, direction):
-	pass
-
-
-#is_on_ledge
-#Checks if entity is on a ledge or not
-#Uses two raycasts downwards to check either its right side or left side is off the platform
+# is_on_ledge
+# Checks if entity is on a ledge or not and returns a bool for the result
+# Uses two raycasts downwards to check either its right side or left side is off the platform
 func is_on_ledge():
 	if ( !left_ray.is_colliding() or !right_ray.is_colliding() ):
 		return true
 	return false
 	
-func aggro_towards(entity):
+func aggro_towards():
 	pass
-	
-func sees_entity(entity): 
-	return false
 	
 func apply_gravity(delta):
 	if not is_on_floor():
 		velocity.y += GameManager.gravity * delta
+
+
+func _on_detect_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group('Player'):
+		sees_player = true
+	else:
+		sees_player = false
