0631371a49368ef6ee737febce0d2dc759aafc8d
cmejaston <colinsipad2@gmail.com>
2025-04-30 13:47:45 -0700

    Updated checkpoint

--- scripts/checkPoint.gd
+++ scripts/checkPoint.gd
@@ -2,6 +2,9 @@ extends Area2D
 #You will need to make a new instance of this code for every checkpoint 
 #Later on, this is where we can add actually saving, but for now this just updates variable in gameManager
 
+@export var scene_path : String
+
+
 func _ready() -> void:
 	pass # Replace with function body.
 
@@ -13,7 +16,7 @@ func _process(delta: float) -> void:
 #Transport to different scene/level. 
 #Updates the "final" versions of coins, health, and score to reflect what they collected in the level
 func _on_body_entered(body: Node2D) -> void:
-	get_tree().change_scene_to_file("res://levels/test_level.tscn")
+	get_tree().change_scene_to_file(scene_path)
 	GameManager.finalCoins = GameManager.coins
 	GameManager.finalHealth = GameManager.health
 	GameManager.finalScore = GameManager.score
